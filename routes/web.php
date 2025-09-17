c053fe86768eab05521ef4efb9e1275464c3211c
danilobogavac <danilobogavac22@gmail.com>
2025-09-17 15:14:15 +0200

    Prvi komit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminAuthController;
+use App\Http\Controllers\Admin\AdminDashboardController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+// Admin
+Route::middleware('admin')->prefix('admin')->group(function () {
+    Route::get('/dashboard',[AdminDashboardController::class,'dashboard'])->name('admin_dashboard');
+    Route::get('/profile',[AdminAuthController::class,'profile'])->name('admin_profile');
+    Route::post('/profile',[AdminAuthController::class,'profile_submit'])->name('admin_profile_submit');
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/login',[AdminAuthController::class,'login'])->name('admin_login');
+    Route::post('/login',[AdminAuthController::class,'login_submit'])->name('admin_login_submit');
+    Route::get('/logout',[AdminAuthController::class,'logout'])->name('admin_logout');
+    Route::get('/forget-password',[AdminAuthController::class,'forget_password'])->name('admin_forget_password');
+    Route::post('/forget-password',[AdminAuthController::class,'forget_password_submit'])->name('admin_forget_password_submit');
+    Route::get('/reset-password/{token}/{email}',[AdminAuthController::class,'reset_password'])->name('admin_reset_password');
+    Route::post('/reset-password/{token}/{email}',[AdminAuthController::class,'reset_password_submit'])->name('admin_reset_password_submit');
+    
+});
